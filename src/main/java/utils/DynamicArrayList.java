27bdadc8aebbf3d4a878c28eca3b6abf26357367
D00258712 <d00258712@student.dkit.ie>
2025-02-13 22:12:12 +0000

    isEmpty sze get

--- src/main/java/utils/DynamicArrayList.java
+++ src/main/java/utils/DynamicArrayList.java
@@ -23,6 +23,22 @@ public class DynamicArrayList {
     public DynamicArrayList() {
         this.array = new String[capacity];
     }
+    public int size(){
+        return numElements;
+    }
 
+    public boolean isEmpty(){
+        if (numElements == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public String get(int index) {
+        if (index < 0 || index >= numElements) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        return array[index];
+    }
 
 }
